bc7ee81f932601ff4c3f7f6fa2df57f4767b2159
Oualid cheriaf <oualid.cheriaf@coddity.com>
2022-12-05 15:12:06 +0100

    feat: Add InMemoryStorage first implementation

---
+++ rpn/storage/inmemory.py
@@ -0,0 +1,33 @@
+from rpn.models.stack import Stack
+from rpn.storage.exceptions import StorageException
+
+
+class InMemoryStorage: 
+    def __init__(self) -> None:
+        self.__db: dict[str, Stack] = dict()
+    
+    def add_stack(self, stack: Stack) -> Stack:
+        if self.stack_exists(stack_id=stack.id):
+            raise StorageException()
+        self.__db[stack.id] = stack
+        return stack
+    
+    def stack_exists(self, stack_id: str) -> bool:
+        if self.__db.get(stack_id, None) is not None:
+            return True
+        return False
+    
+    def get_stack(self, stack_id: str) -> Stack | None:
+        return self.__db.get(stack_id, None)
+    
+    def update_stack(self, stack_id: str, updated_stack: Stack) -> Stack:
+        if not self.stack_exists(stack_id=stack_id) or stack_id != updated_stack.id:
+            raise StorageException()
+        self.__db[stack_id] = updated_stack
+        return updated_stack
+    
+    def delete_stack(self, stack_id: str)-> Stack:
+        if not self.stack_exists(stack_id=stack_id):
+            raise StorageException()
+        deleted_stack = self.__db.pop(stack_id)
+        return deleted_stack
